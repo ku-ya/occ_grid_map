1c820c48e9daad7bbb4f184f1b5315edef192c56
Kuya Takami <kuyachi@gmail.com>
2016-08-31 11:54:57 -0400

    change of forward sensor model fixes noise

--- src/mapping.py
+++ src/mapping.py
@@ -16,7 +16,7 @@ from scipy.stats import norm
 import message_filters
 from bresenham import bresenham
 import gistfile1
-myRes = 0.05
+myRes = 0.1
 
 class Map(object):
     """
@@ -32,8 +32,8 @@ class Map(object):
     with increasing row number.
     """
 
-    def __init__(self, origin_x=0, origin_y=0, resolution=myRes,
-                 width=22, height=22):
+    def __init__(self, origin_x=0., origin_y=0., resolution=myRes,
+                 width=22., height=22.):
         """ Construct an empty occupancy grid.
         Arguments : origin_x, origin_y -- The position of grid cell (0,0) in the
                                 map coordinate frame.
@@ -68,16 +68,16 @@ class Map(object):
 
         # Rotated maps are not supported... quaternion represents no
         # rotation.
-        grid_msg.info.origin = Pose(Point(self.origin_x, self.origin_y, 0),
-                               Quaternion(0, 0, 0, 1))
+        grid_msg.info.origin = Pose(Point(self.origin_x, self.origin_y, 0.),
+                               Quaternion(0., 0., 0., 1.))
 
         # Flatten the numpy array into a list of integers from 0-100.
         # This assumes that the grid entries are probalities in the
         # range 0-1. This code will need to be modified if the grid
         # entries are given a different interpretation (like
         # log-odds).
-        flat_grid = self.grid.reshape((self.grid.size,)) * 100
-        grid_msg.data = list(np.round(flat_grid))
+        flat_grid = self.grid.reshape((self.grid.size,)) * 100.
+        grid_msg.data = list(np.round(flat_grid, decimals = 3))
         return grid_msg
 
     def set_cell(self, x, y, val)      :
@@ -101,18 +101,18 @@ class Mapper(object)                   :
     def __init__(self)                 :
 
         """ Start the mapper. """
-
         rospy.init_node('mapper')
         self._map = Map()
-
         # Setting the queue_size to 1 will prevent the subscriber from
         # buffering scan messages. This is important because the
         # callback is likely to be too slow to keep up with the scan
         # messages. If we buffer those messages we will fall behind
         # and end up processing really old scans. Better to just drop
         # old scans and always work with the most recent available.
-        self.position = [0,0,0]
+        self.position = [0.,0.,0.]
 
+        gridaabb = gistfile1.AABB(np.array([0,0]), np.array([self._map.width,self._map.height]))
+        grid = gistfile1.Grid(gridaabb)
 
         def callback(odom,scan):
             # rospy.Subscriber('odom',
@@ -120,20 +120,19 @@ class Mapper(object)                   :
             # rospy.Subscriber('base_scan_1',
                                     #  LaserScan, self.scan_callback, queue_size=1)
             pos =  odom.pose.pose.position
-            self.position[0] = pos.x
-            self.position[1] = pos.y
+            self.position = pos
             orientation = odom.pose.pose.orientation
             quaternion = (orientation.x,orientation.y,orientation.z, orientation.w)
             euler = tf.transformations.euler_from_quaternion(quaternion)
             roll = euler[0]
             pitch = euler[1]
             yaw = euler[2]
-            self.position[2] =  yaw
-            Lresol = 1/myRes
+            self.rotation =  yaw
+            Lresol = 1./myRes
             r = scan.ranges[0]
-            xt = [self.position[0]+1., self.position[1]+1.0, self.position[2]]
+            xt = [self.position.x+1., self.position.y+1.0, self.rotation]
             # for k in range(0,len(scan.ranges)-1):
-            scan.ranges = scan.ranges
+            # scan.ranges = scan.ranges
             # print scan.ranges
             scanAngles = np.linspace(scan.angle_min,scan.angle_max,len(scan.ranges),-1)
             lidar_local = np.array([xt[0]+scan.ranges*np.cos(scanAngles+xt[2]), xt[1]+(scan.ranges*np.sin(scanAngles+xt[2]))])
@@ -156,8 +155,7 @@ class Mapper(object)                   :
               ray.direction = np.array([np.cos(scanAngles[k]+xt[2]),np.sin(scanAngles[k]+xt[2])])
               ray.norm()
 
-              gridaabb = gistfile1.AABB(np.array([0,0]), np.array([self._map.width,self._map.height]))
-              grid = gistfile1.Grid(gridaabb)
+
               end_pt = rtli
               traversal = gistfile1.AmanatidesTraversal(grid, ray, end_pt, scan.ranges[k]*Lresol)
               voxel = gistfile1.Voxel(grid)
@@ -171,7 +169,6 @@ class Mapper(object)                   :
                             break
                         continue
                     # ray.plot(axes, traversal.get_t_interval()[0], traversal.get_t_interval()[1])
-
                     ray_tmax_y = ray.origin + traversal.get_t_interval()[1]*ray.direction
                     # axes.plot(ray_tmax_y[0],ray_tmax_y[1], 'or', ms=10);
 
@@ -184,10 +181,6 @@ class Mapper(object)                   :
                     if not traversal.step():
                         break
                     pk += 1
-
-                # print path
-                # l.path =  path
-                # l.path = traversal.voxel
                 # l =
                 # if scanAngles[k]>1./2*np.pi or scanAngles[k]<-1./2*np.pi:
                 #     l = bresenham(xtg,rtli)
@@ -201,12 +194,8 @@ class Mapper(object)                   :
                 # l.path = temp
                 # print l.path
                 # for j in range(0,len(l.path)):
-
                 if not len(path)==0:
-                  if scan.ranges[k]<scan.range_max:
-                    self.EISM(path,scan.ranges[k],0)
-                  else:
-                    self.EISM(path, scan.ranges[k],1)
+                  self.EISM(path,scan.ranges[k],scan.ranges[k] < scan.range_max)
 
             # Now that the map is updated, publish it!
             rospy.loginfo("Scan is processed, publishing updated map.")
@@ -236,19 +225,20 @@ class Mapper(object)                   :
     def EISM(self,cell_path,r_s,oorange):
         n = len(cell_path)
         Prtl = np.zeros(n)
+        Pnrtl = np.zeros(n)
         for k in range(0,n-1):
             index = cell_path[k]
             Prtl[k] = self._map.grid[index[1]][index[0]]
-
+        Pnrtl = 1. - Prtl
         # initialize
-        Prtl[0] = 0.0
-        sigma_s = myRes*1.
-        if oorange == 0:
+        Prtl[0] = 0.
+        Pnrtl[0] = 1.
+        sigma_s = myRes*3.
+        # optain Forward sensor model
+        if not oorange:
           pz_xr = self.sensorFM(n,r_s,sigma_s)
         else:
           pz_xr = self.sensorFM(n,r_s*3.,sigma_s*3.)
-        # plt.plot(pz_xr)
-        # plt.show()
 
         a = np.zeros(n)
         b = np.zeros(n)
@@ -256,43 +246,43 @@ class Mapper(object)                   :
         d = np.zeros(n)
         Pr_zxz = np.zeros(n)
         Pnr_zxz = np.zeros(n)
-        for k in range(0,n-1):
+        for k in range(0, n - 1):
             if k == 0:
-                a[0] = 0.0
-                b[0] = 1.0
+                a[0] = 0.
+                b[0] = 1.
                 c[0] = pz_xr[0]
             else:
                 a[k] = a[k-1] + b[k-1]*pz_xr[k-1]*Prtl[k-1]
-                b[k] = b[k-1]*(1-Prtl[k-1])
+                b[k] = b[k-1]*Pnrtl[k-1]
                 c[k] = b[k]*pz_xr[k]
 
-        d[n-1] = 0
-        for k in range(n-2,0,-1):
-            d[k] = d[k+1] + b[k]*pz_xr[k + 1]*Prtl[k + 1]
+        d[n-1] = 0.
+        for k in range(n - 2, 0, -1):
+            d[k] = d[k + 1] + b[k]*pz_xr[k + 1]*Prtl[k + 1]
 
-        for k in range(0,n-1):
+        for k in range(0,n - 1):
             Pr_zxz[k] = a[k] + c[k]
             Pnr_zxz[k] = a[k] + d[k]
 
-        for k in range(0,n-1):
+        for k in range(0, n - 1):
             index = cell_path[k]
             e = Prtl[k]*Pr_zxz[k]
-            f = (1.0-Prtl[k])*Pnr_zxz[k]
-            # if e == 0:
-            #   e = 1e-12
-            # if f == 0:
-            #   f = 1e-12
-
-            if not np.isnan(e/(e+f)):
-              if oorange == 0:
+            f = Pnrtl[k]*Pnr_zxz[k]
+            if not (e+f) == 0.: #np.isnan(e/(e+f)):
+              if oorange:
                 self._map.grid[index[1]][index[0]] = e/(e+f)
               else:
-                self._map.grid[index[1]][index[0]] = 0.01
+                self._map.grid[index[1]][index[0]] = 0.1
+            else:
+              if not oorange:
+                self._map.grid[index[1]][index[0]] = 0.1
 
     def sensorFM(self,n,r_s,sigma_s):
-        x = np.linspace(0,r_s,n)
-        return norm.pdf(x,loc=r_s,scale=sigma_s)
-
+        x = np.linspace(0.,r_s,n)
+        model = norm.pdf(x,loc=r_s,scale=sigma_s)
+        # return model
+        model[0:n-3] = 0.
+        return model*5.
 
     def publish_map(self):
         """ Publish the map. """
@@ -300,6 +290,7 @@ class Mapper(object)                   :
         self._map_data_pub.publish(grid_msg.info)
         self._map_pub.publish(grid_msg)
         # rospy.signal_shutdown("stop spin")
+
     def odom_callback(self,odom):
         # global myOdom = odom
         pos =  odom.pose.pose.position
--- src/move_robot.py
+++ src/move_robot.py
@@ -10,20 +10,21 @@ def keyboard():
     rospy.init_node('teleop_py',anonymous=True)
     rate = rospy.Rate(10)
     k = 1
-    while not rospy.is_shutdown() and k < 300:
+    while not rospy.is_shutdown() and k < 250:
       twist.linear.x = 1.0
-      twist.angular.z = 0.02
+      twist.angular.z = 0.01
       twist.linear.y = 0.0
       pub.publish(twist)
       rate.sleep()
       k +=1
     k = 1
-    while not rospy.is_shutdown() and k < 10:
-      twist.linear.x = 0.0
-      twist.angular.z = 0.1
-      twist.linear.y = 0.0
-      pub.publish(twist)
-      rate.sleep()
+    # while not rospy.is_shutdown() and k < 400:
+    #   twist.linear.x = 0.0
+    #   twist.angular.z = 0.1
+    #   twist.linear.y = 0.0
+    #   pub.publish(twist)
+    #   rate.sleep()
+    #   k +=1
 
 
 
